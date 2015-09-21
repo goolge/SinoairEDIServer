2510983f3cdce4dcb02fd612b7304cc838cf0e37
zhangmj <zhangmj@sinoair.com>
2015-09-21 17:10:07 +0800

    add rm traces

--- src/main/java/com/sinoair/iemisgateway/royalMail/action/ReceiveRmTracesAction.java
+++ src/main/java/com/sinoair/iemisgateway/royalMail/action/ReceiveRmTracesAction.java
@@ -1,5 +1,9 @@
 package com.sinoair.iemisgateway.royalMail.action;
 
+import com.sinoair.iemisgateway.royalMail.service.ReceiveRmTracesService;
+import com.sinoair.iemisgateway.util.ConnectionFactory;
+import com.sinoair.iemisgateway.util.PropertiesUtil;
+
 /**
  * Created by IntelliJ IDEA.
  * User: ZhangMJ
@@ -8,8 +12,9 @@ package com.sinoair.iemisgateway.royalMail.action;
  * To change this template use File | Settings | File Templates.
  */
 public class ReceiveRmTracesAction {
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws Exception{
+          ReceiveRmTracesService receiveRmTracesService=new ReceiveRmTracesService();
+          receiveRmTracesService.ReceiveTraces(ConnectionFactory.getConnectionInProperties(), PropertiesUtil.readProperty("common", "historyRootPath"));
     }
 
 }
