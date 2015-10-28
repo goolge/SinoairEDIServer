7d2d7f5256764375092e2515ee8d405f0a23aae6
zhangmj <zhangmj@sinoair.com>
2015-10-28 16:04:46 +0800

    sql optimize

--- src/main/java/com/sinoair/iemisgateway/cainiao/service/PushTrace2CainiaoService.java
+++ src/main/java/com/sinoair/iemisgateway/cainiao/service/PushTrace2CainiaoService.java
@@ -160,15 +160,31 @@ public class PushTrace2CainiaoService {
 
     private static ResultSet getResultSet(Statement statement) throws SQLException {
         String date = PropertiesUtil.readProperty("cainiao", "date");
-        String sql = "SELECT EBA_SYSCODE,EP.EAWB_PRINTCODE,EAWB_SERVICETYPE,EAWB_REFERENCE1,EAWB_REFERENCE2,EBA_OCCURTIME,EBA_REMARK,EBA.EBA_OCCURPLACE,EBA.EAD_CODE,EBA.EAST_CODE,EAT.EAT_PARTNER_ACTIVITY_DESC,EAT.EAT_PARTNER_ACTIVITY_CODE\n  FROM EAWBPRE EP, EXPRESSBUSINESSACTIVITY EBA,EXPRESSACTIVITYTRANSLATE EAT\n" +
-                " WHERE EP.EAWB_PRINTCODE = EBA.EAWB_PRINTCODE\n" +
-                " AND EAT.EAD_CODE=EBA.EAD_CODE\n" +
-                " AND EAT.EAST_CODE=EBA.EAST_CODE\n" +
-                " AND EAT.EAT_PARTNER_ID='cainiao'\n" +
-                " AND (EBA.QA IS NULL OR EBA.QA <> 's')\n" +
+       String sql = "SELECT " +
+                "EBA.EBA_SYSCODE," +
+                "EP.EAWB_PRINTCODE," +
+                "EP.EAWB_SERVICETYPE," +
+                "EP.EAWB_REFERENCE1," +
+                "EP.EAWB_REFERENCE2," +
+                "EBA.EBA_OCCURTIME," +
+                "EBA.EBA_REMARK," +
+                "EBA.EBA_OCCURPLACE," +
+                "EBA.EAD_CODE," +
+                "EBA.EAST_CODE," +
+                "EAT.EAT_PARTNER_ACTIVITY_DESC," +
+                "EAT.EAT_PARTNER_ACTIVITY_CODE\n  " +
+                " FROM " +
+                " EAWBPRE EP," +
+                " EXPRESSBUSINESSACTIVITY EBA," +
+                " EXPRESSACTIVITYTRANSLATE EAT\n" +
+                " WHERE EBA.EAWB_PRINTCODE = EP.EAWB_PRINTCODE\n" +
+                " AND EBA.EAD_CODE = EAT.EAD_CODE\n" +
+                " AND EAT.EAST_CODE = EBA.EAST_CODE\n" +
                 " AND EP.EAWB_SO_CODE = '00060491'\n" +
-                " AND EP.EAWB_HANDLETIME > " + date;//todo 为了提高查询效率，只查找最近两个月处理的单子
-        BaseLogger.info("查询最近两个月所有的需要发给菜鸟的轨迹:\n" + sql);
+                " AND (EBA.QA IS NULL OR EBA.QA <> 's')\n" +
+                " AND EAT.EAT_PARTNER_ID = 'cainiao'\n" +
+                " AND EBA.EBA_HANDLETIME > " + date;
+        BaseLogger.info("查询最近 "+date+" 所有的需要发给菜鸟的轨迹:\n" + sql);
         ResultSet resultSet = statement.executeQuery(sql);
         return resultSet;
     }
--- src/main/java/com/sinoair/iemisgateway/royalMail/service/SendRmManifestMailService.java
+++ src/main/java/com/sinoair/iemisgateway/royalMail/service/SendRmManifestMailService.java
@@ -25,27 +25,33 @@ public class SendRmManifestMailService {
      * @throws Exception
      */
     public Map<String, String> getSendMialInfo(Connection conn) throws Exception {
-        String aheadDateStr=DateUtil.getDateStrAheadDays(-1,"yyyy-MM-dd")+" 00:00:00";
+        String aheadDateStr2=DateUtil.getDateStrAheadDays(-2,"yyyy-MM-dd")+" 23:59:59";
+        String aheadDateStr1=DateUtil.getDateStrAheadDays(-1,"yyyy-MM-dd")+" 23:59:59";
 
         String sql = "select nvl(count(esa.pkg_printcode),0) pkgNum," + //1
-                " nvl(sum(esa.pkg_actualweight),0) pkgWeight" +//2
+                " nvl(sum(esa.pkg_actualweight),0) pkgWeight," +//2
+                " nvl(sum(data.pkgInnerNum),0) pkgInnerNum" +//3
                 " from expresssortpackagedetail esa," +
-                " (select ed.pkg_parentcode" +
+                " (select ed.pkg_parentcode,count(ed.pkg_printcode) pkgInnerNum" +
                 " from expresssortpackagedetail ed, express_manifest em" +
                 " where ed.pkg_printcode = em.eawb_printcode" +
-                " and em.em_partner = '"+RoyalMailManifest.PARTNER_CODE+"' and em.em_handletime >to_date('"+aheadDateStr+"','yyyy-mm-dd hh24:mi:ss') " +
+                " and em.em_partner = '"+RoyalMailManifest.PARTNER_CODE+"' " +
+                " and em.em_handletime >to_date('"+aheadDateStr2+"','yyyy-mm-dd hh24:mi:ss') " +
+                " and em.em_handletime <=to_date('"+aheadDateStr1+"','yyyy-mm-dd hh24:mi:ss') " +
                 " group by ed.pkg_parentcode) data" +
                 " where esa.pkg_printcode = data.pkg_parentcode " ;
         PreparedStatement selectPstm = conn.prepareStatement(sql);
-         //LogUtil.log(" 英邮标准小包发邮件："+sql);
+         LogUtil.log(" 英邮标准小包发邮件："+sql);
             ResultSet rs = selectPstm.executeQuery();
            Map<String, String> map=new HashMap<String,String>();
             map.put("pkgNum","0");
             map.put("pkgWeight","0");
+            map.put("pkgInnerNum","0");
             if (rs.next()) {
                 map.put("pkgNum",rs.getString(1)+"");
                 map.put("pkgWeight",rs.getString(2)+"");
-                 //LogUtil.log(" 英邮标准小包邮件数据--大包数量："+rs.getString(1)+" 大包重量："+rs.getString(2));
+                map.put("pkgInnerNum",rs.getString(3)+"");
+                 LogUtil.log(" 英邮标准小包邮件数据--大包数量："+rs.getString(1)+"总运单数："+rs.getString(3)+" 大包重量："+rs.getString(2));
             }
          ConnectionFactory.closeConnection(conn);
         return map;
@@ -53,15 +59,16 @@ public class SendRmManifestMailService {
     public  void sendEmail(Connection conn) throws Exception{
         Map<String,String> map=getSendMialInfo(conn);
         int pkgNum=Integer.parseInt(map.get("pkgNum"));
+        int pkgInnerNum=Integer.parseInt(map.get("pkgInnerNum"));
         double pkgWeight=Double.parseDouble(map.get("pkgWeight"));
         //LogUtil.log(" 22英邮标准小包邮件数据--大包数量："+pkgNum+" 大包重量："+pkgWeight);
         if(pkgNum>0){
             MailUtil.postMail(
-                    PropertiesUtil.readProperty("royalMail", "rmMailTo"),
-                    PropertiesUtil.readProperty("royalMail", "rmMailCc"),
-                   PropertiesUtil.readProperty("royalMail", "rmMailBcc"),
+                    "xiaoyan@sinoair.com",//PropertiesUtil.readProperty("royalMail", "rmMailTo")
+                    "zhangmj@sinoair.com",// PropertiesUtil.readProperty("royalMail", "rmMailCc")
+                    "WangXX4@sinoair.com",// PropertiesUtil.readProperty("royalMail", "rmMailBcc")
                     "the coming mail number to LON",
-                    "Packets number: "+pkgNum+" ,Packets weight: "+pkgWeight+" kg, Expected arriving time: 24 hours later",
+                    "Packets number: "+pkgNum+" ,Parcel number:"+pkgInnerNum+" ,Packets weight: "+pkgWeight+" kg, Expected arriving time: 24 hours later",
                     "iemis@sinoair.com",
                     null,
                     "iemis",
